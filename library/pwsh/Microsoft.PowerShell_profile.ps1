74434e442635f804e6a06c7432bb4fc10f8e3aff
S. Davis <sdavis@nativeit.net>
2024-11-03 22:22:28 -0500

    Added powershell and vscode

---
+++ library/pwsh/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/gruvbox.omp.json" | Invoke-Expression
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/stelbent.minimal.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/takuya.omp.json" | Invoke-Expression
